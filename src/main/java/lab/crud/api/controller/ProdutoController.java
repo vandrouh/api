acf2df26f1cc6e598382532e94e8fa111c201dfb
Alessandro Tacini <alessandro.tacini@fieb.edu.br>
2025-10-03 22:52:54 -0300

    Add metodo buscarPorId

--- src/main/java/lab/crud/api/controller/ProdutoController.java
+++ src/main/java/lab/crud/api/controller/ProdutoController.java
@@ -1,11 +1,13 @@
 package lab.crud.api.controller;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,4 +47,26 @@ public class ProdutoController {
 				.status(HttpStatus.OK)
 				.body(repository.findAll());
 	}
+	
+	@GetMapping("/produtos/{id}")
+	public ResponseEntity<Object> buscarPorId(@PathVariable Integer id) {
+
+		// Alt + SHIFT + L -> extrai variável local
+		Optional<Produto> produtoEncontrado = repository.findById(id);
+
+		// Empty = Vazio
+		if (produtoEncontrado.isEmpty()) {
+			return ResponseEntity
+					.status(HttpStatus.NOT_FOUND)
+					.body("Produto não encontrado");
+		}
+
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(produtoEncontrado.get());
+	}
+	
+	
+	
+	
 }
